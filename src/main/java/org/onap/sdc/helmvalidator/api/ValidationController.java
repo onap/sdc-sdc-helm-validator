260d45eda2e7026391a330bdfa43c706cf1e5927
Piotr Marcinkiewicz <piotr.marcinkiewicz@nokia.com>
2021-04-21 15:30:05 +0200

    Add Swagger and OpenAPI description
    Issue-ID: SDC-3185
    Signed-off-by: Piotr Marcinkiewicz <piotr.marcinkiewicz@nokia.com>
    Change-Id: Ib6371ec21ad8331124efd1fd15d2e82364c10e39

--- src/main/java/org/onap/sdc/helmvalidator/api/ValidationController.java
+++ src/main/java/org/onap/sdc/helmvalidator/api/ValidationController.java
@@ -20,19 +20,30 @@
 
 package org.onap.sdc.helmvalidator.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.onap.sdc.helmvalidator.errorhandling.ValidationErrorResponse;
 import org.onap.sdc.helmvalidator.helm.validation.ValidationService;
 import org.onap.sdc.helmvalidator.helm.validation.model.ValidationResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@Tag(name = "ValidationService")
 public class ValidationController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ValidationController.class);
@@ -53,11 +64,27 @@ public class ValidationController {
      * @param isStrictLinted flag deciding if chart should be linted with strict option turned on
      * @return Response with result of validation
      */
-    @PostMapping("/validate")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Helm chart successfully validated",
+            content = @Content(schema = @Schema(implementation = ValidationResult.class))),
+        @ApiResponse(responseCode = "400", description = "Chart cannot be validated using selected version",
+            content = @Content(schema = @Schema(implementation = ValidationErrorResponse.class))),
+        @ApiResponse(responseCode = "500", description = "Something went wrong during validation execution",
+            content = @Content(schema = @Schema(implementation = ValidationErrorResponse.class)))
+    })
+    @Operation(
+        summary = "Validate chart",
+        description = "Web endpoint for Helm charts validation.",
+        tags = "ValidationService")
+    @PostMapping(value = "/validate", produces = "application/json", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ValidationResult> validate(
+        @Parameter(description = "Desired Helm version which should be used to validate the chart")
         @RequestParam(value = "versionDesired", required = false) String version,
-        @RequestParam("file") MultipartFile file,
+        @Parameter(description = "Helm chart that should be validated (packed in .tgz format)", required = true)
+        @RequestPart MultipartFile file,
+        @Parameter(description = "If false, there will be an attempt to render the chart without linting it first")
         @RequestParam(value = "isLinted", required = false, defaultValue = "false") boolean isLinted,
+        @Parameter(description = "Linting should be strict or not")
         @RequestParam(value = "isStrictLinted", required = false, defaultValue = "false") boolean isStrictLinted) {
         LOGGER.debug("Received file: {}, size: {}, helm version: {}",
             file.getOriginalFilename(), file.getSize(), version);
